4c70bdbe831bc86031e2c6d9f264b423c4a29eae
shahzadnabil98-ship-it <shahzadnabil98@gmail.com>
2026-05-06 10:25:16 +0200

    kleine Änderung

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/' , (req, res) => {
-    return res.json ({ msg:  'Hello from the server v1' })
+    return res.json ({ msg:  'Hello from the server von Nabil' })
 })
 
 app.listen(PORT, '0.0.0.0', () => {
